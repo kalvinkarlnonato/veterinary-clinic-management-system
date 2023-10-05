f7b2922c75af82c93ab41ac0568fd8c33d981736
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2023-10-05 09:25:22 +0800

    New! Reports form

---
+++ VCMS/Forms/ReportsForm.cs
@@ -0,0 +1,28 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VCMS.Forms
+{
+    public partial class ReportsForm : MaterialForm
+    {
+
+        MaterialSkinManager skinManager;
+        public ReportsForm()
+        {
+            InitializeComponent();
+            skinManager = MaterialSkinManager.Instance;
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            skinManager.ColorScheme = new ColorScheme(Primary.Blue600, Primary.Blue900, Primary.Blue900, Accent.LightBlue200, TextShade.BLACK);
+        }
+    }
+}
